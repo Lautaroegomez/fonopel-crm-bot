13d0100520ae4e4534d83771249456f1502a0ca2
Lautaroegomez <LautaroElias.Gomez@alumnos.uai.edu.ar>
2026-03-23 19:16:49 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+{
+  "name": "fonopel-crm-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "dependencies": {
+    "@google/generative-ai": "^0.3.0",
+    "express": "^4.18.2",
+    "body-parser": "^1.20.2"
+  }
+}
